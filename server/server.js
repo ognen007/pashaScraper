5f4446c0a5ac5358009a19f3ed91570372e122ba
Ognen Veljanovski <150041811+ognenPCG@users.noreply.github.com>
2024-04-08 18:43:13 +0200

    chrome fix

--- server/server.js
+++ server/server.js
@@ -86,7 +86,7 @@ const exportToZip = async (data, res) => {
     });
 };
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
